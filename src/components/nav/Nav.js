3916992ff932fc714ebf009a31d36adaa12d2c20
Pawel Bareja <bareja.pawel@gmail.com>
2021-01-30 18:56:12 +0100

    fix mobile menu

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect, useCallback } from "react"
 // import { Link } from "gatsby"
 import { AnchorLink } from "gatsby-plugin-anchor-links"
+import { HamburgerSlider } from "react-animated-burgers"
 
 // css
 import "./nav.scss"
@@ -12,7 +13,11 @@ import burgerDark from "../../assets/images/icons/menu3.svg"
 const Nav = () => {
   const [menu, showMenu] = useState(true)
   const [mobileMenu, showMobileMenu] = useState(false)
-  const [hovered, sethovered] = useState(false)
+
+  const toggleButton = useCallback(
+    () => showMobileMenu(prevState => !prevState),
+    []
+  )
 
   const toggleMenu = e => {
     let distanceFromTop = window.pageYOffset
@@ -59,26 +64,40 @@ const Nav = () => {
       <div className="logo" href="/">
         <span>LOGO</span>
       </div>
-      <span className="burgerMenu" onClick={toggleMobileMenu}>
+      {/* <span className="burgerMenu" onClick={toggleMobileMenu}>
         <img src={burgerDark} alt="burger menu" />
         <span>menu</span>
-      </span>
+      </span> */}
+      <HamburgerSlider
+        // buttonColor="#FFBC67"
+        barColor={"#444"}
+        className="burgerMenu"
+        isActive={mobileMenu}
+        {...{ mobileMenu, toggleButton }}
+      />
       <div className={mobileMenu ? "navbar--active" : "navbar"}>
         <AnchorLink
           to="/#benefits"
           className="nav--link"
           activeClassName="activeLink"
+          onAnchorLinkClick={() => showMobileMenu(false)}
         >
           Co nas wyróżnia
         </AnchorLink>
 
-        <AnchorLink to="/#about" className="stripped" stripHash>
+        <AnchorLink
+          to="/#about"
+          className="nav--link"
+          activeClassName="activeLink"
+          onAnchorLinkClick={() => showMobileMenu(false)}
+        >
           O nas
         </AnchorLink>
         <AnchorLink
           className="nav--link"
           to="/#gallery"
           activeClassName="activeLink"
+          onAnchorLinkClick={() => showMobileMenu(false)}
         >
           Galeria
         </AnchorLink>
@@ -86,6 +105,7 @@ const Nav = () => {
           className="nav--link nav--link-custom"
           to="/#contact"
           activeClassName="activeLink"
+          onAnchorLinkClick={() => showMobileMenu(false)}
         >
           Kontakt
         </AnchorLink>
